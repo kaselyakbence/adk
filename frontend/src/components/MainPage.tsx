88b18e5fd3b9e3161bb578f0da26e0f1f24998d9
kaselyakbence <kaselyakbence.dev@gmail.com>
2025-02-01 00:30:42 +0100

    Initial design

--- frontend/src/components/MainPage.tsx
+++ frontend/src/components/MainPage.tsx
@@ -1,13 +1,24 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { DevicesContext } from "../context/DevicesContext";
 import styles from "./mainpage.module.css";
 import Countdown from "./countdown/Countdown";
+import { MdCameraswitch } from "react-icons/md";
+import TimerModal from "../modals/TimerModal";
 
 const MainPage = () => {
+  // const [isModalOpen, setIsModalOpen] = useState(true);
+  const [chosenDevice, setChosenDevice] = useState<string | null>(null);
+
   const devices = useContext(DevicesContext);
 
   return (
     <>
+      <div className={styles.header}>
+        <p className={styles.header_text}>ADK Washing</p>
+        <div className={styles.circle}>
+          <MdCameraswitch className={styles.icon} />
+        </div>
+      </div>
       <div className={styles.washers}>
         <div className={styles.washer_header}>
           <p className={styles.washer_header_left}>Washing machines</p>
@@ -17,7 +28,11 @@ const MainPage = () => {
           devices
             .filter((d) => d.type == "washer")
             .map((d) => (
-              <div className={styles.item} key={d.id}>
+              <div
+                className={styles.item}
+                key={d.id}
+                onClick={() => setChosenDevice(d.name)}
+              >
                 <div>{d.name}</div>
                 <Countdown />
               </div>
@@ -32,12 +47,17 @@ const MainPage = () => {
           devices
             .filter((d) => d.type == "dryer")
             .map((d) => (
-              <div className={styles.item} key={d.id}>
+              <div
+                className={styles.item}
+                key={d.id}
+                onClick={() => setChosenDevice(d.name)}
+              >
                 <div>{d.name}</div>
                 <Countdown />
               </div>
             ))}
       </div>
+      <TimerModal device={chosenDevice} setIsOpen={setChosenDevice} />
     </>
   );
 };
--- frontend/src/components/countdown/Countdown.tsx
+++ frontend/src/components/countdown/Countdown.tsx
@@ -3,7 +3,7 @@ import ReactCountdown from "react-countdown";
 const Countdown = () => {
   return (
     <ReactCountdown
-      date={Date.now() + 100000}
+      date={Date.now() + 10000000}
       renderer={({ hours, minutes, seconds }) => (
         <span>
           {hours}:{minutes}:{seconds}
