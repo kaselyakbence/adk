346bd2d7e88969096066e6ea66a5d437e93089e2
kaselyakbence <kaselyakbence.dev@gmail.com>
2025-02-08 19:28:20 +0100

    Backend setup

---
+++ backend/src/index.ts
@@ -0,0 +1,5 @@
+import { add } from "./utils";
+
+console.log(add(1, 2));
+
+console.log(process.env.APP_DEBUG);
---
+++ backend/tests/add.test.ts
@@ -0,0 +1,3 @@
+import { add } from "../src/utils";
+
+it("add 2numbers", () => expect(add(1, 2)).toBe(3));
